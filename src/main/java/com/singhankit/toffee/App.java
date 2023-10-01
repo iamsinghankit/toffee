4a1809bad52a29d1f6b9b62bd825ab085a0afd49
iamsinghankit <34397119+iamsinghankit@users.noreply.github.com>
2023-10-01 19:16:50 +0530

    project setup

---
+++ src/main/java/com/singhankit/toffee/App.java
@@ -0,0 +1,16 @@
+package com.singhankit.toffee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author Ankit Singh
+ */
+class App {
+    private static final Logger LOG = LoggerFactory.getLogger( App.class );
+
+    void main() {
+        LOG.info( "Hello World" );
+    }
+}
